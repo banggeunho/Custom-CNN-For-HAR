f5fd7058e6c76e330afe389f1c5dc4e7de10fd90
banggeunho <panggeunho@gmail.com>
2022-05-05 06:08:28 +0900

    add sliding window(Data augmented)

--- dataloader.py
+++ dataloader.py
@@ -21,16 +21,25 @@ class CustomDataset(Dataset):
             # print(file)
             try:
                 df = pd.read_csv(file)
+                # 지정해준 segement값 만큼 데이터를 가져와서, 라벨링 해줍니다.
                 for i in range(0, len(df.index), data_seg):
-                    if i+data_seg > len(df.index):
+                    if i+data_seg > len(df.index): # 지정해준 값을 현재 인덱스에 더했을때 csv의 길이보다 초과하면 break
                         break
-                    temp_x = df.values[i:i+data_seg]
+                    temp = df[i:i+data_seg]
+                    temp_acc = temp.iloc[:, 0:3].transpose()
+                    temp_acc = np.expand_dims(temp_acc, axis=0)
+                    temp_gyro = temp.iloc[:, 3:].transpose()
+                    temp_gyro = np.expand_dims(temp_gyro, axis=0)
+                    temp_x = np.concatenate([temp_acc, temp_gyro], axis=0)
+                    print(temp_x.shape)
+                    # print(temp_x)
+                    # 파일이름에서 level(label)을 가져옵니다.
                     if file[-6] == '_':
                         temp_y = file[-5:-4]
                     else:
                         temp_y = file[-6:-4]
-                    self.data.append((temp_x, int(temp_y)-1))
-                    # print((temp_x, int(temp_y)))
+                    self.data.append((temp_x, int(temp_y)-1)) # self.data list에 추가합니다.
+                    print((temp_x, int(temp_y)))
             except ValueError:
                 continue
 
@@ -39,7 +48,7 @@ class CustomDataset(Dataset):
 
     def __getitem__(self, idx):
         x, y = self.data[idx]
-        x = np.expand_dims(x, axis=0)
+        # x = np.expand_dims(x, axis=0) # CNN 모델에 적용시키기 위해 차원(채널)을 하나 더 늘려줍니다.
         x = torch.FloatTensor(x)
         # y = torch.IntTensor(y)
         return x, y
