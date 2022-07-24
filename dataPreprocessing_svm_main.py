8315141457b465266cace52d3cf8806a648fb339
banggeunho <panggeunho@gmail.com>
2022-07-24 16:16:22 +0900

    수집데이터 전처리 메인모듈(SVM 컬럼 추가 버전)

---
+++ dataPreprocessing_svm_main.py
@@ -0,0 +1,32 @@
+from dataPreprocessing_svm_1 import january_preprocessing
+from dataPreprocessing_svm_2 import february_preprocessing
+import os
+from train_test_split import train_val_test_split
+
+total_length = 0
+
+
+save_path = f'./svm_output_ss_1+2/'
+path_1 = './backup/1월/'
+path_2 = './backup/2월/'
+train_rate = 0.6
+val_rate = 0.2
+num_classes = 13
+
+print(save_path)
+os.makedirs(save_path, exist_ok=True)
+
+# 레벨별로 output
+for i in range(1, 14):
+    os.makedirs(save_path + str(i), exist_ok=True)
+
+total_length += january_preprocessing(path_1, save_path)
+total_length += february_preprocessing(path_2, save_path)
+
+train_val_test_split(save_path, num_classes, train_rate, val_rate)
+
+
+print(f'Total length : {total_length}')
+print('Done!')
+
+
